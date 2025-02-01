cbd322d67d53356ef6270769e624f5d194a1ea7b
FrancescoGiommi <francescogiommi97@icloud.com>
2025-02-01 17:44:10 +0100

    Add fix

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -1,13 +1,24 @@
-export default function CardProduction({ production }) {
-  const flagLanguage = () => {
-    if (production.original_language === "es") return "ES";
-    if (production.original_language === "pt") return "PT";
-    if (production.original_language === "sv") return "SV";
-    if (production.original_language === "hi") return "HI";
-    if (production.original_language === "it") return "IT";
-    if (production.original_language === "en") return "EN";
-  };
-  const flags = flagLanguage();
+const flagLanguage = (language) => {
+  switch (language) {
+    case "es":
+      return "ES";
+    case "pt":
+      return "PT";
+    case "sv":
+      return "SV";
+    case "hi":
+      return "HI";
+    case "it":
+      return "IT";
+    case "en":
+      return "EN";
+    default:
+      return "EN";
+  }
+};
+
+export default function CardMovie({ production }) {
+  const flags = flagLanguage(production.original_language);
 
   const posterImage = (poster) => {
     const imgUrl = "https://image.tmdb.org/t/p";
@@ -15,55 +26,47 @@ export default function CardProduction({ production }) {
     return imgUrl + imgSize + poster;
   };
 
-  const voteConverter = (oldScore) => {
-    const newScore = Math.round(oldScore / 2);
-    return newScore;
-  };
+  const voteConverter = (oldScore) => Math.round(oldScore / 2);
 
   const renderStars = (score) => {
     const stars = [];
     for (let i = 0; i < 5; i++) {
-      if (i < score) {
-        stars.push(<i key={i} className="fas fa-star"></i>);
-      } else {
-        stars.push(<i key={i} className="far fa-star"></i>);
-      }
+      stars.push(
+        <i key={i} className={i < score ? "fas fa-star" : "far fa-star"}></i>
+      );
     }
     return stars;
   };
 
-  const oldScore = production.vote_average;
-  const newScore = voteConverter(oldScore);
+  const newScore = voteConverter(production.vote_average);
   return (
-    <>
-      <div className="col-4 card-container ">
-        <div className="image-container">
-          <img
-            src={posterImage(production.poster_path)}
-            alt={`Poster di ${production.title}`}
-          />
-          <div className="overlay">
-            <ul>
-              <li>
-                <b>Titolo:</b> {production.title}
-              </li>
-              <li>
-                <b>Titolo originale:</b> {production.original_title}
-              </li>
-              <li>
-                <b>Lingua:</b> {production.original_language}
-                <img
-                  src={`https://flagsapi.com/${flags}/shiny/64.png`}
-                  alt="Bandiera"
-                />
-              </li>
-              <li>
-                <b>Punteggio:</b> {renderStars(newScore)}
-              </li>
-            </ul>
-          </div>
+    <div className="col-4 card-container">
+      <div className="image-container">
+        <img
+          src={posterImage(production.poster_path)}
+          alt={`Poster di ${production.title}`}
+        />
+        <div className="overlay">
+          <ul>
+            <li>
+              <b>Titolo:</b> {production.title}
+            </li>
+            <li>
+              <b>Titolo originale:</b> {production.original_title}
+            </li>
+            <li>
+              <b>Lingua:</b> {production.original_language}
+              <img
+                src={`https://flagsapi.com/${flags}/shiny/64.png`}
+                alt="Bandiera"
+              />
+            </li>
+            <li>
+              <b>Punteggio:</b> {renderStars(newScore)}
+            </li>
+          </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 }
